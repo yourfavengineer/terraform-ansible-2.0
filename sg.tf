ac3a8528b5954709fc3b8de43e7084c8a63c1e0b
Kendall Kehbuma <126034797+yourfavengineer@users.noreply.github.com>
2024-02-14 16:37:03 -0800

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "allow_tls" {
+  name = "terraform-sg"
+
+  ingress {
+    description = "Allow remote SSH from anywhere"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  # enable HTTP
+  ingress {
+    description = "Allow HTTP request from anywhere"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+  ingress {
+    description = "Allow HTTP request from anywhere"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  # enable HTTPS
+  ingress {
+    description = "Allow HTTPS request from anywhere"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+
+  # Outgoing request
+  egress {
+    description = "Allow outgoing request"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security Groups to allow SSH(22), HTTP(8080), and HTTPS(443) for nginx access"
+  }
+}
